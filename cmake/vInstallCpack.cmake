5fa8c9007e581d0bef99ac20fd41d7cd966df82a
caesar <caesar@kekxv.com>
2020-09-26 16:20:44 +0800

    更新目录结构，增加 flash 问文件存储位置。

--- cmake/vInstallCpack.cmake
+++ cmake/vInstallCpack.cmake
@@ -1,6 +1,6 @@
 #vInstallCpack.cmake
 ###cpack
-string(TIMESTAMP vTimeStamp "%Y%m%d%H%M%S")
+string(TIMESTAMP vTimeStamp "%Y%m%d%H%M")
 
 set(CPACK_GENERATOR "TGZ")
 set(CPACK_PACKAGE_FILE_NAME "${PROJECT_NAME}-${vTimeStamp}")
